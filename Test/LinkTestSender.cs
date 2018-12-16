4034bae84d8c38f4b8d58cf2192b2b58ea995f7d
bertflemming <33054467+bertflemming@users.noreply.github.com>
2018-12-16 20:49:28 +0100

    yees

---
+++ Test/LinkTestSender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Linklaget;
+using System.Text;
+
+/// <summary>
+/// Transport.
+/// </summary>
+namespace TestSender
+{
+    /// <summary>
+    /// Transport.
+    /// </summary>
+    public class LinkTestSender
+    {
+
+		private LinkTestSender()
+        {
+            Link link = new Link(1004, "TEST");
+            byte[] buffer = Encoding.ASCII.GetBytes("AXBY");
+			link.send(buffer, buffer.Length);
+        }
+
+        public static void Main(string[] args)
+        {
+			Console.WriteLine("LinkTestSender starts...");
+			Console.WriteLine("Sending AXBY");
+
+			new LinkTestSender();
+        }
+    }
+}
---
+++ TestRec/LinkTestRecieve.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Linklaget;
+using System.Text;
+
+/// <summary>
+/// Transport.
+/// </summary>
+namespace TestRecieve
+{
+    /// <summary>
+    /// Transport.
+    /// </summary>
+    public class LinkTestRecieve
+    {
+        int size = 0;
+        private LinkTestRecieve()
+        {
+            Console.WriteLine("Hej");
+            Link link = new Link(1004, "TEST");
+            byte[] buffer = new byte[1000];
+            size = link.receive(ref buffer);
+            Console.WriteLine($"Size: {size} Buffer: {Encoding.ASCII.GetString(buffer)}");
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("LinkTestRecieve starts...");
+            new LinkTestRecieve();
+        }
+    }
+}
